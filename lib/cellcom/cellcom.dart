4d7f0d4a0e5d78548c79c7e2cf491435a968ab88
Anderson Goumou <121631954+4n-d3er-git@users.noreply.github.com>
2024-03-26 21:35:19 +0000

    Update cellcom.dart

--- lib/cellcom/cellcom.dart
+++ lib/cellcom/cellcom.dart
@@ -20,9 +20,8 @@ class _CELLCOMState extends State<CELLCOM> {
     InterstitialAd.load(
       
       adUnitId: 
-      "ca-app-pub-7229654893754092/9010044563",
       //test
-      // "ca-app-pub-3940256099942544/1033173712",
+      "ca-app-pub-3940256099942544/1033173712",
       request: const AdRequest(),
       adLoadCallback: InterstitialAdLoadCallback(
         onAdLoaded: (ad) {
