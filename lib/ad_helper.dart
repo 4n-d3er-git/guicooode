0f842fb44d84f236660b855330dc5a88d17bbfbb
Anderson Goumou <121631954+4n-d3er-git@users.noreply.github.com>
2024-03-26 21:31:53 +0000

    Update ad_helper.dart

--- lib/ad_helper.dart
+++ lib/ad_helper.dart
@@ -4,8 +4,7 @@ class AdHelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
       // vous pouvez le remplacer par le votre
-      // return 'ca-app-pub-3940256099942544/6300978111'; //test
-      return 'ca-app-pub-7229654893754092/5344589732';
+      return 'ca-app-pub-3940256099942544/6300978111'; //test
     } else if (Platform.isIOS) {
      return 'ca-app-pub-3940256099942544/2934735716'; //test
     } else {
